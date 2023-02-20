c54fc745537c17b52f05e53dcaa18455e7559a2b
pallav saikia <pallav.saikia@styldod.com>
2023-02-20 21:41:53 +0530

    added project filter

--- api/project.api.js
+++ api/project.api.js
@@ -6,6 +6,36 @@ const successResponse = require('../helper/response_wrapper/success_res')
 const server_code = require('../helper/server_code')
 const isStringEmpty = require('../helper/isStringEmpty')
 
+router.get('/', async (req, res, next) => {
+
+    const page = (req.body.page || 1);
+    const size = 5;
+    const skip = size * (page - 1);
+    let totalPage = 0;
+
+    const query = {};
+    const sort = { createdAt: -1 }
+    if (req.body.name) { query['name'] = req.body.name }
+    if (req.body.status) { query['status'] = req.body.status }
+    if (req.body.sort === 'asc') { sort['createdAt'] = 1 }
+
+    try {
+        const [project, totalCount] = await Promise.all([ProjectSchema.find(query).sort(sort).limit(size).skip(skip).lean(),
+            ProjectSchema.find(query).count()
+        ])
+   
+        if (totalCount) {
+            console.log(totalCount)
+            totalPage = Math.ceil(totalCount/ size)
+        }
+        return new successResponse(res, { totalpage: totalPage,currentpage:page, project: project }, "fetched")
+    } catch (err) {
+        return next(new Error(err.message));
+    }
+})
+
+
+
 
 router.post('/', async (req, res, next) => {
 
