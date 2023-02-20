72bed8de165ed50faf6f36ac2af1e54afebb2719
pallav saikia <pallav.saikia@styldod.com>
2023-02-20 22:28:01 +0530

    added all

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-assignment desc
+-clone project
+-git install
+-install nodemon--- npm install nodemon
+-npm start 
+
+
+signup
+http://127.0.0.1:3000/api/signup
+post
+[name: name,
+email: email,
+phone: phone,
+gender: gender]
+
+
+password reset
+http://127.0.0.1:3000/api/passwordreset/{jwt}
+get[link from email]
+post[
+    queryparam:req.params.jwt,
+    password:password
+]
+
+
+
+login api
+http://127.0.0.1:3000/api/login
+post[
+    email or phone:loginid,
+    password:password
+]
+
+
+project api
+http://127.0.0.1:3000/api/project
+headers[Authorization:jwt]
+
+get[
+    name:name,
+    status:status,
+    sort(createdAt):sort ['asc','dsc'],
+    page number:page
+]
+
+post [
+    name:name
+    status:status
+    description:description
+]
+
+
+task api
+http://127.0.0.1:3000/api/task
+headers[Authorization:jwt]
+
+get[
+    name:name,
+    status:status,
+    priority:priority,
+    projectid:projectid
+    sort(createdAt):sort ['asc','dsc'],
+    page number:page
+]
+
+post [
+    name: name,
+    description: description,
+    project id to add to: project,
+    priority: priority,
+    status: status
+]
--- api/task.api.js
+++ api/task.api.js
@@ -7,6 +7,36 @@ const successResponse = require('../helper/response_wrapper/success_res')
 const server_code = require('../helper/server_code')
 const isStringEmpty = require('../helper/isStringEmpty')
 
+router.get('/', async (req, res, next) => {
+
+    const page = (req.body.page || 1);
+    const size = 5;
+    const skip = size * (page - 1);
+    let totalPage = 0;
+
+    const query = {};
+    const sort = { createdAt: -1 }
+    if (req.body.name) { query['name'] = req.body.name }
+    if (req.body.status) { query['status'] = req.body.status }
+    if (req.body.priority) { query['priority'] = req.body.priority }
+    if (req.body.projectid) { query['project'] = req.body.projectid }
+    if (req.body.sort === 'asc') { sort['createdAt'] = 1 }
+    console.log(query)
+    try {
+        const [task, totalCount] = await Promise.all([TaskSchema.find(query).populate('project', { '_id': 1, 'name': 1, 'status': 1 }).sort(sort).limit(size).skip(skip).lean(),
+        TaskSchema.find(query).count()
+        ])
+
+        if (totalCount) {
+            console.log(totalCount)
+            totalPage = Math.ceil(totalCount / size)
+        }
+        return new successResponse(res, { totalpage: totalPage, currentpage: page, task: task }, "fetched")
+    } catch (err) {
+        return next(new Error(err.message));
+    }
+})
+
 
 router.post('/', async (req, res, next) => {
 
@@ -14,7 +44,7 @@ router.post('/', async (req, res, next) => {
         return new errorResponse(res, 'Fields cant be empty', server_code._500)
     }
     try {
-        console.log(req.body.projectid )
+        console.log(req.body.projectid)
         const project = await ProjectSchema.findOne({ _id: req.body.projectid }).lean()
         if (project) {
             const task = await new TaskSchema({
